7ab08618889e33f15399b280df821d9b2b339df8
baob <coder@onesandthrees.com>
2015-07-25 00:56:09 +0100

    tidy up routes

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,5 @@
-post '/books', to: 'books#create'
-get '/books/new', to: 'books#new'
-get '/books', to: 'books#index'
 # Configure your routes here
 # See: http://www.rubydoc.info/gems/lotus-router/#Usage
 
-get '/', to: 'home#index'
+resources :books
+get '/', to: 'home#index', as: :home
