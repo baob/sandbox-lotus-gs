9efe0c72b2a6b7004d55ab7d450067037fceb829
baob <coder@onesandthrees.com>
2015-07-24 21:29:10 +0100

    switch to progress bar

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -2,10 +2,13 @@
 require_relative './spec_helper'
 
 require 'capybara'
+
 require 'capybara/dsl'
 require 'minitest/reporters'
 
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+#Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
+#Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 Capybara.app = Lotus::Container.new
 
